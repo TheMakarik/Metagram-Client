b7416a91a5ee120138073470cd0ba2670a3e6555
themakarik <themakarik@gmail.com>
2026-03-02 00:19:09 +0700

    2 Strange folders deleted

--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Metagram.ViewModels
-{
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting { get; } = "Welcome to Avalonia!";
-    }
-}
--- ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Metagram.ViewModels
-{
-    public abstract class ViewModelBase : ReactiveObject
-    {
-    }
-}
